f70e02fd6c4f00285498ae69a02d8194d484268e
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2024-11-19 23:22:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+
+const delay =(ms)=> new Promise(resolve => setTimeout(resolve,ms));
+async function displaymessage() {
+	 const text=document.getElementById('text').value.trim();
+     const delayTime=parseInt(document.getElementById('delay').value);
+     const output=document.getElementById('output');
+	
+	output.textContent='';
+	if(!text||isNaN(delayTime)||delayTime<0){
+		output.textContent = 'Please enter valid text and a non-negative delay.';
+        return;
+	}
+	await delay(delayTime)
+	output.textContent=text;
+}
+
+
+
+document.getElementById('btn').addEventListener("click",displaymessage);
